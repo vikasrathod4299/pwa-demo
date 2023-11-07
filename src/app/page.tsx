ada1ea45d80fe1a4797b493f016dbc3acccbfcf7
vikasrathod4299 <vikas.rv4299@gmail.com>
2023-11-07 17:08:12 +0530

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Vikas rathod
+      PMTC health care website
     </main>
   );
 }
